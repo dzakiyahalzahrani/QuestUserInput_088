ea06913c276f2f31d54ac9fe192fd876e589da95
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-30 12:41:15 +0700

    menambahkan modifier pada HorizontalDivider kedua

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -98,7 +98,9 @@ fun FormDataDiri(modifier: Modifier)
             Text(stringResource(R.string.submit))
         }
         HorizontalDivider(
-
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
         )
 
 
