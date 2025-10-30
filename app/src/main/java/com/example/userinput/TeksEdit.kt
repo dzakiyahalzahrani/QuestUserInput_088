cc716dd9cf56b17892d9ab3c3b8382afa621ef1d
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-30 12:41:54 +0700

    menambahkan thickness pada HorizontalDivider kedua

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -101,7 +101,9 @@ fun FormDataDiri(modifier: Modifier)
             modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
                 id = R.dimen.padding_medium
             )),
-        )
+            thickness = dimensionResource(R.dimen.divider_tipis),
+
+            )
 
 
 
