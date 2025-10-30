6271ea26617778c1c123c47480ede4ce87a53e68
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-30 11:56:44 +0700

    membuat fun FormDataDiri

---
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -0,0 +1,35 @@
+package com.example.userinput
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf( "")}
+    var textJK by remember {mutableStateOf("")}
+
+    var nama by remember {mutableStateOf("")}
+    var alamat by remember {mutableStateOf("")}
+    var jenis by remember {mutableStateOf("")}
+
+    var gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    
+
+}
